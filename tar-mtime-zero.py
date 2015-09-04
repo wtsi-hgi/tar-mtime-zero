0f03170102152dab7bc5a94dda91f627224fc0c4
Joshua Randall <joshua.randall@sanger.ac.uk>
2015-09-04 01:04:09 +0100

    Python script to zero out all mtimes in tar
    Streams tar from stdin to stdout, setting all mtime values
    to zero along the way. Useful for "purifying" tar files
    such as the output of `docker save`

---
+++ tar-mtime-zero.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import tarfile
+from sys import stdin, stdout, stderr
+
+tar_in = tarfile.open(fileobj=stdin, mode="r|")
+tar_out = tarfile.open(fileobj=stdout, mode="w|", format=tarfile.PAX_FORMAT)
+
+for tarinfo in tar_in:
+    tarinfo.mtime = 0
+    tar_out.addfile(tarinfo, fileobj=tar_in.extractfile(tarinfo))
+
+tar_in.close()
+tar_out.close()
